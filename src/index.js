5ee56c4972417445ee6cc41e103a5ec4fe79aad9
RobRoy001 <robertsteuer@hotmail.de>
2026-08-29 09:35:58 +0000

    fix: CORS konfigurieren für Port 5173

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 const pool = require('./database/connection');
 const authRouter = require('./api/auth');
